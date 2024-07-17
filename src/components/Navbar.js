1ae1c954936801083eb4af6d8454135007320d52
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-17 20:40:54 +0300

    update material UI

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+       <div className="toggleButton">
+       <button>
+       
+       </button>
+       </div>
+    </div>
+  )
+}
+
+export default Navbar
