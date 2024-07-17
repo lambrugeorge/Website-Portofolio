29c698fb7ebc2c233e42fbe98db456d59de5ffa4
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-17 20:48:46 +0300

    install updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
