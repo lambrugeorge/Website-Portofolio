2fb6d190389cce83d6f4d29f3156ad7e7ac48bf5
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-20 02:18:25 +0300

    created Projects and Experience section

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -4,19 +4,43 @@ import {
   VerticalTimelineElement
 } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
-import { School } from '@material-ui/icons'
+import { School, Work } from '@material-ui/icons'
+import '../styles/Experience.css'
+
+
 
 function Experience() {
   return (
     <div className='experience'>
       <VerticalTimeline lineColor='#3e497a'>
         <VerticalTimelineElement 
+          className='vertical-timeline-element--work'
+          date="2010 - 2014"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<Work />}
+        >
+        <h3 className='vertical-timeline-element-title'> My Random Highschool, Random Place, Random State</h3>
+        <p> High School</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement 
           className='vertical-timeline-element--education'
-          date="2010-2014"
+          date="2010 - 2014"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<School />}
         >
+        <h3 className='vertical-timeline-element-title'> My Random Highschool, Random Place, Random State</h3>
+        <p> High School</p>
         </VerticalTimelineElement>
+        <VerticalTimelineElement 
+          className='vertical-timeline-element--education'
+          date="2010 - 2014"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<School />}
+        >
+        <h3 className='vertical-timeline-element-title'> My Random Highschool, Random Place, Random State</h3>
+        <p> High School</p>
+        </VerticalTimelineElement>
+        
       </VerticalTimeline>
     </div>
   )
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
+import Proj1 from '../assets/Proj1.png';
+import Proj2 from '../assets/Proj2.png';
+import '../styles/Projects.css'
+
+
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <h1> My Personal Projects</h1>
+      <div className="projectList">
+      <ProjectItem name="Social Media Website" image={Proj1}/>
+      <ProjectItem name="Social Media Website" image={Proj2}/>
+      </div>
+    </div>
   )
 }
 
